43b550120dc4a43eb09e85ab7fd06e0a74e070fc
csicsi2000 <69510072+csicsi2000@users.noreply.github.com>
2021-05-04 17:07:46 +0200

    s5 breaking

---
+++ index.js
@@ -0,0 +1,25 @@
+var options = {
+    root: document.querySelector('#scrollArea'),
+    rootMargin: '0px',
+    threshold: 0.1
+}
+var count = 0;
+const callback = (entries, observer) => {
+    entries.forEach((entry) => {
+        console.log(count);
+      if (entry.isIntersecting) {
+        entry.target.classList.add("fadeIn")
+      }
+    })
+  }
+
+const observer = new IntersectionObserver(callback, options);
+
+
+
+var target= document.querySelectorAll(".project");
+for(i =0; i< target.length; i++){
+    observer.observe(target[i]);
+}
+
+
