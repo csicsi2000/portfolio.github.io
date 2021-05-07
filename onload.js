83290ae819ec470cbbe060de842e607fb680c49e
csicsi2000 <69510072+csicsi2000@users.noreply.github.com>
2021-05-07 14:13:06 +0200

    mobile view fixed

--- onload.js
+++ onload.js
@@ -17,3 +17,10 @@ window.onscroll = function() {
     }
   prevScrollpos = currentScrollPos;
 }
+
+$( document ).ready(function () {
+  let viewheight = $(window).height();
+  let viewwidth = $(window).width();
+  let viewport = document.querySelector("meta[name=viewport]");
+  viewport.setAttribute("content", "height=" + viewheight + "px, width=" + viewwidth + "px, initial-scale=1.0");
+}, 300);
